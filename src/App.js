c3db28250f99a8ea7cb09661a07df88848202629
SteveGuoh <hghandle@outlook.com>
2023-11-02 16:41:22 +0800

    feat: basic HTML page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('https://jsonplaceholder.typicode.com/users');
+      setContacts(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contacts</h1>
+      {contacts.map(contact => (
+        <div key={contact.id}>
+          <h2>{contact.name}</h2>
+          <p>{contact.email}</p>
+          <p>Username: {contact.username}</p>
+          <p>Phone: {contact.phone}</p>
+          <p>Website: {contact.website}</p>
+          <p>Company: {contact.company.name}</p>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
